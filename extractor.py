346f739c89fb104e4269c12fb4d7d3da6c838a97
Dark Hole <dark.hole1@yandex.ru>
2021-06-14 23:01:14 +0300

    Minor fix

--- extractor.py
+++ extractor.py
@@ -68,4 +68,4 @@ def _add_letter(points, letter):
     return list(map(lambda x: (*x, letter), points))
 
 def dist(p1, p2):
-    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]+p2[1])**2)
+    return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
